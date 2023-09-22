8c1ba47ac11bcc1058619449ed60dc0a7f2dff82
Osamagamal1911 <73962760+Osamagamal1911@users.noreply.github.com>
2023-09-22 16:34:17 +0300

    Update installNginx.groovy

--- vars/installNginx.groovy
+++ vars/installNginx.groovy
@@ -1,15 +1,1 @@
-pipeline {
-    agent {
-        label 'slave'
-    }
 
-    stages {
-        stage('Install Nginx') {
-            steps {
-                script {
-                    shared_library.installNginx()
-                }
-            }
-        }
-    }
-}
